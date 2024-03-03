4af13f1ca127e31bb840fad53a87572138952b5e
David Pavlic <david.pavlic0@gmail.com>
2024-03-03 20:59:55 +0100

    included chatgpt api to summarize the fetched content

--- newsScraper/inspection.go
+++ newsScraper/inspection.go
@@ -4,23 +4,26 @@ import (
 	"fmt"
 	"github.com/gocolly/colly"
 	"log"
+	"strings"
 )
 
-func inspectArticle(c *colly.Collector, articleUrl string) {
+func inspectArticle(c *colly.Collector, articleUrl string) (string, error){
+	var articleContent strings.Builder
+
 	c.OnHTML("h1.article__title", func(element *colly.HTMLElement) {
-		fmt.Printf("Title: ")
-		fmt.Println(element.Text)
+		articleContent.WriteString("Title: " + element.Text + "\n")
 	})
 
 	c.OnHTML("div.article-content p", func(element *colly.HTMLElement) {
 		if element.DOM.Parent().Is("div.article-content") && element.Attr("class") != "wp-caption-text" {
-			fmt.Println(element.Text)
+			articleContent.WriteString(element.Text)
 		}
 	})
 
 	err := c.Visit(articleUrl)
 	if err != nil {
 		log.Printf("failed to visit url: %v\n", err)
-		return
+		return "", fmt.Errorf("failed to visit url: %v", err)
 	}
+	return articleContent.String(), nil
 }
--- newsScraper/main.go
+++ newsScraper/main.go
@@ -29,12 +29,27 @@ func main() {
 		case "2":
 			articleUrl = getLatestArticle(c)
 		default:
-			fmt.Print("Invalid option\n>")
+			fmt.Print("Invalid option\n")
 			displayOptions() // Remind the user of the options if they input an invalid option
 			continue
 		}
 		break
 	}
 	setUp(articleUrl)
-	inspectArticle(c, articleUrl)
+
+	articleContent, err := inspectArticle(c, articleUrl)
+	if err != nil {
+		fmt.Printf("Error inspecting article: %v\n", err)
+		return
+	} else {
+		fmt.Printf(articleContent + "\n\n\n\n\n\n\n\n")
+	}
+
+	// Now summarize the content using ChatGPT
+	summary, err := SummarizeContent(articleContent)
+	if err != nil {
+		fmt.Printf("Error summarizing content: %v\n", err)
+	} else {
+		fmt.Println("Summary:", summary)
+	}
 }
